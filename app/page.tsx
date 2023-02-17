e0c9a42cb397b7fb1d37ddb7276f33383756b954
chris <chris@afox.cc>
2023-02-17 11:27:00 +0800

    Update index page

--- app/page.tsx
+++ app/page.tsx
@@ -39,24 +39,26 @@ export default function Home() {
   }
 
   return (
-    <div className='pt-20'>
-      <div className='flex flex-col justify-center items-center'>
-        <h1 className='text-5xl mb-6 font-bold'>Hello Lens 🌿</h1>
-    
-        {profiles.map(profile => (
-          <div key={profile.id} className='w-2/3 shadow-md p-6 rounded-lg mb-8 flex flex-col items-center'>
-            <img className='w-48 h-48 rounded-full' src={profile.avatarUrl || 'https://picsum.photos/200'} />
-            <p className='text-xl text-center mt-6'>{profile.name}</p>
-            <p className='text-base text-gray-400 text-center mt-2'>{profile.bio}</p>
-            
-            <Link href={`/${profile.handle}`}>
-              <p className='cursor-pointer text-violet-600 text-lg font-medium text-center mt-2 mb-2'>{profile.handle}</p>
-            </Link>
+    <div className='pt-20 max-w-screen-md mx-auto'>
+      <div className='pt-20 container mx-auto'>
+        <div className='flex flex-wrap justify-center'>
+          {profiles.map((profile, index) => (
+            <div key={profile.id} className={`w-full md:w-1/2 shadow-md p-6 rounded-lg mb-8 flex flex-col items-center ${index % 2 === 0 ? "md:pr-4" : "md:pl-4"}`}>
+              <img className='w-48 h-48 rounded-full' src={profile.avatarUrl || 'https://picsum.photos/200'} />
+              <p className='text-xl text-center mt-6'>{profile.name}</p>
+              <p className='text-base text-gray-400 text-center mt-2'>{profile.bio}</p>
 
-            <p className='text-pink-600 text-sm font-medium text-center'>{profile.stats.totalFollowers} followers</p>
-          </div>
-        ))}
+              <Link href={`/${profile.handle}`}>
+                <p className='cursor-pointer text-violet-600 text-lg font-medium text-center mt-2 mb-2'>{profile.handle}</p>
+              </Link>
+
+              <p className='text-pink-600 text-sm font-medium text-center'>{profile.stats.totalFollowers} followers</p>
+            </div>
+          ))}
+        </div>
       </div>
+
     </div>
+
   )
 }
