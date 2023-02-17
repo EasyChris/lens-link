73f3fd4d275754bd271bf78c903f17b82a9552f3
chris <chris@afox.cc>
2023-02-17 15:18:53 +0800

    Added header to page

--- app/page.tsx
+++ app/page.tsx
@@ -17,6 +17,15 @@ import './styles.css';
 export default function Home() {
   const [profiles, setProfiles] = useState<any>([]);
 
+  const gradients = [
+    'linear-gradient(to bottom right, #fa709a, #fee140)',
+    'linear-gradient(to bottom right, #fa709a, #fee140)',
+    // 'linear-gradient(to bottom right, #b92b27, #1565c0)',
+    // add more gradients here
+  ];
+
+  const [selectedGradient, setSelectedGradient] = useState(gradients[Math.floor(Math.random() * gradients.length)]);
+
   useEffect(() => {
     fetchProfiles();
   }, []);
@@ -125,50 +134,40 @@ export default function Home() {
     // 更新profiles 
 
     useEffect(() => {
-    profiles[index].isFollowedByMe = true;
-    setProfiles([...profiles]);
+      profiles[index].isFollowedByMe = true;
+      setProfiles([...profiles]);
     }, []);
-    
+
   }
 
   return (
-    <div className='pt-20 max-w-screen-md mx-auto'>
-      <button onClick={login}>登录</button>
-
-      <header className='text-3xl font-bold text-center mb-10'>LINK MORE LENSER</header>
-      <div className='pt-20 container mx-auto'>
-        <div className='grid grid-cols-1 md:grid-cols-2 gap-8'>
-          {profiles.map((profile, index) => (
-            <Link href={`/${profile.handle}`} key={profile.id}>
-              <div key={profile.id} className='shadow-md p-6 rounded-lg flex flex-col items-center'>
-                {/* 如果isFollow为false显示关注按钮 */}
-              {profile.isFollowedByMe ? (
-                <p className="text-sm text-gray-400 text-center">Following</p>
-              ) : (
-                // 点击触发followed 并传入profile 作为参数
-                <button
-                  onClick={() => followed(profile, index)}
-                  className="bg-violet-600 text-white text-sm font-medium px-4 py-2 rounded-full"
-                >
-                  Follow
-                </button>
-              )}
-                <img className='w-48 h-48 rounded-full' src={profile.avatarUrl || 'https://picsum.photos/200'} />
-                <p className='text-xl text-center mt-6'>{profile.name}</p>
-                <div className='h-16'>
-                  <p className='text-base text-gray-400 text-center mt-2 overflow-hidden line-clamp-2'>{profile.bio}</p>
+    <div className="App" style={{ backgroundImage: selectedGradient }}>
+      <header className='text-3xl font-bold text-center mb-10 bg-white text-white flex justify-between items-center py-3 px-6'>
+        <div className='text-white text-black'>LINKLINK</div>
+        <button className='bg-gradient-to-r from-gray-800 to-black text-white text-sm px-4 py-2 rounded-md' onClick={login}>登录</button>
+      </header>
+      <div className='pt- max-w-screen-md mx-auto'>
+        <div className='pt-5 container mx-auto'>
+          <div className='grid grid-cols-1 md:grid-cols-2 gap-8'>
+            {profiles.map((profile, index) => (
+              <Link href={`/${profile.handle}`} key={profile.id}>
+                <div key={profile.id} className='shadow-md p-6 rounded-lg flex flex-col items-center' style={{ backgroundColor: 'white' }}>
+                  <img className='w-48 h-48 rounded-full' src={profile.avatarUrl || 'https://picsum.photos/200'} />
+                  <p className='text-xl text-center mt-6'>{profile.name}</p>
+                  <div className='h-16'>
+                    <p className='text-base text-gray-400 text-center mt-2 overflow-hidden line-clamp-2'>{profile.bio}</p>
+                  </div>
+                  <p className='text-pink-600 text-sm font-medium text-center mt-auto'>
+                    <FaUsers className="inline-block mr-2" />
+                    {profile.stats.totalFollowers} followers
+                  </p>
                 </div>
-                <p className='text-pink-600 text-sm font-medium text-center mt-auto'>
-                  <FaUsers className="inline-block mr-2" />
-                  {profile.stats.totalFollowers} followers
-                </p>
-              </div>
-            </Link>
-          ))}
+              </Link>
+            ))}
+          </div>
         </div>
       </div>
     </div>
-
 
   )
 }
