97ea8037c1ad00ea0fe95e946f6105459b25ba87
north <1340652578@qq.com>
2023-02-17 14:46:48 +0800

    added auth

--- api.ts
+++ api.ts
@@ -1,11 +1,33 @@
-import { ApolloClient, InMemoryCache, gql } from '@apollo/client'
+import { ApolloClient, InMemoryCache, gql, ApolloLink, from, HttpLink } from '@apollo/client'
 
 const API_URL = 'https://api.lens.dev'
 // const API_URL = 'https://api-mumbai.lens.dev'
+const authLink = new ApolloLink((operation, forward) => {
+  // Use the setContext method to set the HTTP headers.
+  let token: any = window.sessionStorage.getItem('token');
+  if (token) {
+    token = JSON.parse(token)
+    console.log(token)
 
+  }
+  operation.setContext({
+    headers: {
+      'x-access-token': token ? `Bearer ${token.accessToken}` : '',
+      'origin': 'https://lenster.xyz',
+      'referer': 'https://lenster.xyz/',
+      'sec-fetch-mode': 'cors',
+      'sec-fetch-site': 'same-site',
+      'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36'
+    },
+  });
+
+  // Call the next link in the middleware chain.
+  return forward(operation);
+});
 /* create the API client */
 export const client = new ApolloClient({
-  uri: API_URL,
+  link: from([authLink, new HttpLink({ uri: API_URL })]),
+
   cache: new InMemoryCache()
 })
 
@@ -17,20 +39,93 @@ query ExploreProfiles {
       id
       name
       bio
+      isDefault
+      attributes {
+        displayType
+        traitType
+        key
+        value
+      }
+      followNftAddress
+      isFollowedByMe
+      isFollowing
+      metadata
       handle
       picture {
+        ... on NftImage {
+          contractAddress
+          tokenId
+          uri
+          chainId
+          verified
+        }
         ... on MediaSet {
           original {
             url
+            mimeType
           }
         }
       }
+      coverPicture {
+        ... on NftImage {
+          contractAddress
+          tokenId
+          uri
+          chainId
+          verified
+        }
+        ... on MediaSet {
+          original {
+            url
+            mimeType
+          }
+        }
+      }
+      ownedBy
+      dispatcher {
+        address
+        canUseRelay
+      }
       stats {
         totalFollowers
+        totalFollowing
+        totalPosts
+        totalComments
+        totalMirrors
+        totalPublications
+        totalCollects
       }
+      followModule {
+        ... on FeeFollowModuleSettings {
+          type
+          contractAddress
+          amount {
+            asset {
+              name
+              symbol
+              decimals
+              address
+            }
+            value
+          }
+          recipient
+        }
+        ... on ProfileFollowModuleSettings {
+        type
+        }
+        ... on RevertFollowModuleSettings {
+        type
+        }
+      }
+    }
+    pageInfo {
+      prev
+      next
+      totalCount
     }
   }
 }
+
 `
 
 // export const getProfile = gql`
@@ -112,3 +207,99 @@ export const getPublications = gql`
     content
   }
 `
+export const generateChallenge = gql`
+query Challenge($request: ChallengeRequest!) {
+  challenge(request: $request) {
+    text
+  }
+}`
+export const getToken = gql`mutation authenticate($request: SignedAuthChallenge!) {
+  authenticate(request: $request) {
+    accessToken
+    refreshToken
+  }
+}`
+export const getDefaultProfile = gql`query DefaultProfile($address: EthereumAddress!) {
+  defaultProfile(request: { ethereumAddress: $address}) {
+    id
+    name
+    bio
+    isDefault
+    attributes {
+      displayType
+      traitType
+      key
+      value
+    }
+    followNftAddress
+    metadata
+    handle
+    picture {
+      ... on NftImage {
+        contractAddress
+        tokenId
+        uri
+        chainId
+        verified
+      }
+      ... on MediaSet {
+        original {
+          url
+          mimeType
+        }
+      }
+    }
+    coverPicture {
+      ... on NftImage {
+        contractAddress
+        tokenId
+        uri
+        chainId
+        verified
+      }
+      ... on MediaSet {
+        original {
+          url
+          mimeType
+        }
+      }
+    }
+    ownedBy
+    dispatcher {
+      address
+      canUseRelay
+    }
+    stats {
+      totalFollowers
+      totalFollowing
+      totalPosts
+      totalComments
+      totalMirrors
+      totalPublications
+      totalCollects
+    }
+    followModule {
+      ... on FeeFollowModuleSettings {
+        type
+        contractAddress
+        amount {
+          asset {
+            name
+            symbol
+            decimals
+            address
+          }
+          value
+        }
+        recipient
+      }
+      ... on ProfileFollowModuleSettings {
+       type
+      }
+      ... on RevertFollowModuleSettings {
+       type
+      }
+    }
+  }
+}`
+export const follow = gql`mutation ProxyAction($request: ProxyActionRequest!) {\n  proxyAction(request: $request)\n}`
--- app/page.tsx
+++ app/page.tsx
@@ -1,53 +1,158 @@
 /* app/page.tsx */
-'use client'
+"use client";
 
-import { useEffect, useState } from 'react'
-import { client, exploreProfiles } from '../api'
-import Link from 'next/link'
+import { useEffect, useState } from "react";
+import {
+  client,
+  exploreProfiles,
+  generateChallenge,
+  getToken,
+  getDefaultProfile,
+  follow,
+} from "../api";
+import Link from "next/link";
 import { FaUsers } from 'react-icons/fa';
 import './styles.css';
 
 export default function Home() {
-  const [profiles, setProfiles] = useState<any>([])
+  const [profiles, setProfiles] = useState<any>([]);
 
   useEffect(() => {
-    fetchProfiles()
-  }, [])
+    fetchProfiles();
+  }, []);
 
   async function fetchProfiles() {
     try {
-      let response = await client.query({ query: exploreProfiles })
+      let response = await client.query({ query: exploreProfiles });
 
-      let profileData = await Promise.all(response.data.exploreProfiles.items.map(async profileInfo => {
-        let profile = { ...profileInfo }
-        let picture = profile.picture
+      let profileData = await Promise.all(
+        response.data.exploreProfiles.items.map(async (profileInfo) => {
+          let profile = { ...profileInfo };
+          let picture = profile.picture;
 
-        if (picture && picture.original && picture.original.url) {
-          if (picture.original.url.startsWith('ipfs://')) {
-            let result = picture.original.url.substring(7, picture.original.url.length)
-            profile.avatarUrl = `http://lens.infura-ipfs.io/ipfs/${result}`
-          } else {
-            profile.avatarUrl = picture.original.url
+          if (picture && picture.original && picture.original.url) {
+            if (picture.original.url.startsWith("ipfs://")) {
+              let result = picture.original.url.substring(
+                7,
+                picture.original.url.length
+              );
+              profile.avatarUrl = `http://lens.infura-ipfs.io/ipfs/${result}`;
+            } else {
+              profile.avatarUrl = picture.original.url;
+            }
           }
-        }
 
-        return profile
-      }))
+          return profile;
+        })
+      );
 
-      setProfiles(profileData)
+      setProfiles(profileData);
     } catch (err) {
-      console.log({ err })
+      console.log({ err });
     }
+  }
+  async function login() {
+    let w = window as any;
+    if (typeof w.ethereum == "undefined") {
+      console.log("MetaMask is installed!");
+      return;
+    }
+    const accounts = await w.ethereum.request({
+      method: "eth_requestAccounts",
+    });
+    const address = accounts[0];
+
+    let response = await client.query({
+      query: generateChallenge,
+      variables: {
+        request: {
+          address,
+        },
+      },
+    });
+    // 调用MetaMask签名
+    const signature = await w.ethereum.request({
+      method: "personal_sign",
+      params: [response.data.challenge.text, address],
+    });
+    // apollo 通过签名和地址获取token
+    const result = await client.mutate({
+      mutation: getToken,
+      variables: {
+        request: {
+          address,
+          signature,
+        },
+      },
+    });
+    window.sessionStorage.setItem(
+      "token",
+      JSON.stringify(result.data.authenticate)
+    );
+    //
+    const defaultProfile = await client.query({
+      query: getDefaultProfile,
+      variables: {
+        address,
+      },
+    });
+    window.sessionStorage.setItem(
+      "user",
+      JSON.stringify(defaultProfile.data.defaultProfile)
+    );
+  }
+  async function followed(profile: any, index: number) {
+    console.log("followed");
+    let user: any = window.sessionStorage.getItem("user");
+    //如果为空 提示登录 否则 parse成对象
+    if (!user) {
+      alert("请先登录");
+      return;
+    }
+    console.log(profile);
+    await client.mutate({
+      mutation: follow,
+      variables: {
+        request: {
+          follow: {
+            freeFollow: {
+              profileId: profile.id,
+            },
+          },
+        },
+      },
+    });
+    // 更新profiles 
+
+    useEffect(() => {
+    profiles[index].isFollowedByMe = true;
+    setProfiles([...profiles]);
+    }, []);
+    
   }
 
   return (
     <div className='pt-20 max-w-screen-md mx-auto'>
+      <button onClick={login}>登录</button>
+
       <header className='text-3xl font-bold text-center mb-10'>LINK MORE LENSER</header>
       <div className='pt-20 container mx-auto'>
         <div className='grid grid-cols-1 md:grid-cols-2 gap-8'>
           {profiles.map((profile, index) => (
             <Link href={`/${profile.handle}`} key={profile.id}>
               <div key={profile.id} className='shadow-md p-6 rounded-lg flex flex-col items-center'>
+                {/* 如果isFollow为false显示关注按钮 */}
+              {profile.isFollowedByMe ? (
+                <p className="text-sm text-gray-400 text-center">Following</p>
+              ) : (
+                // 点击触发followed 并传入profile 作为参数
+                <button
+                  onClick={() => followed(profile, index)}
+                  className="bg-violet-600 text-white text-sm font-medium px-4 py-2 rounded-full"
+                >
+                  Follow
+                </button>
+              )}
                 <img className='w-48 h-48 rounded-full' src={profile.avatarUrl || 'https://picsum.photos/200'} />
                 <p className='text-xl text-center mt-6'>{profile.name}</p>
                 <div className='h-16'>
